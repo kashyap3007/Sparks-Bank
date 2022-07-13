5f02e800e88c679e26dda175a4bdffdf779a3852
kashyap3007 <rajayush3007@gmail.com>
2022-07-13 09:39:48 +0530

    Completed

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+let myAccountBalance = parseInt(
+  document.getElementById("myAccountBalance").innerText
+);
+
+let temp = myAccountBalance;
+// Account Balance
+
+function sendMoney() {
+  var enterName = document.getElementById("enterName").value;
+  var enterAmount = parseInt(document.getElementById("enterAmount").value);
+
+  if (enterAmount > 10000) {
+    alert("You Can only send Rs 10000 at a time.");
+  } else if (enterAmount > temp) {
+    alert("Insufficient Balance.");
+  } else {
+    var findUserBankAccount = enterName + "BankBalance";
+    var finalAmount =
+      parseInt(document.getElementById(findUserBankAccount).innerHTML) +
+      enterAmount;
+    var myAccountBalance =
+      parseInt(document.getElementById("myAccountBalance").innerText) -
+      enterAmount;
+    document.getElementById("myAccountBalance").innerText = myAccountBalance;
+    temp = myAccountBalance;
+    document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+    alert(`Successful Transaction !!  
+      $${enterAmount} is sent to ${enterName}@gmail.com.`);
+
+    // transaction history
+    var createPTag = document.createElement("li");
+    var textNode = document.createTextNode(
+      `$${enterAmount} is sent to recepient with Email-id ${enterName}@gmail.com on ${Date()}.`
+    );
+    createPTag.appendChild(textNode);
+    var element = document.getElementById("transaction-history-body");
+    element.insertBefore(createPTag, element.firstChild);
+  }
+}
